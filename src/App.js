536b0fdca6a3edd71fe98f9713cd278e2b89c783
EBEREGIT <samsoneberenjoku@gmail.com>
2020-08-11 00:39:41 +0100

    navigation and header complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Styles/App.scss";
 import NavComponent from "./Components/Header/NavComponent";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <>
       <NavComponent />
+      <Header />
     </>
   );
 }
--- src/Components/Header/NavComponent.js
+++ src/Components/Header/NavComponent.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
 
 export default function NavComponent() {
-    return (
-        <>
-             <h1>Nav</h1>
-        </>
-    )
+  return (
+    <>
+      <Navbar expand="lg" className="wrapper">
+        {/* logo */}
+        <img
+          src="https://res.cloudinary.com/dunksyqjj/image/upload/v1597094132/LogoMakr_6cGYpQ_h58asa.png"
+          width="35"
+          height="50"
+          className="d-inline-block align-top"
+          alt="React Bootstrap logo"
+        />
+        {/* <Navbar.Brand href="#home">IdeasHub</Navbar.Brand> */}
+
+        {/* search form */}
+        <Form inline>
+          <FormControl
+            type="text"
+            placeholder="Search"
+            className="mr-sm-2 shadow-none"
+          />
+          <Button>Search</Button>
+        </Form>
+
+        {/* toggle button */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        {/* collapseable menu */}
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            {/* menu */}
+            <Nav.Link href="#home">About</Nav.Link>
+            <Nav.Link href="#link">Contact</Nav.Link>
+            <Nav.Link href="#link">Add Project</Nav.Link>
+
+            {/* Account */}
+            <NavDropdown title="Account" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
 }
