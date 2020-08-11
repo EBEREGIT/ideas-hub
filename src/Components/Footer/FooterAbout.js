55082a0b2b3b490f397411387bd9f0b365bb044f
EBEREGIT <samsoneberenjoku@gmail.com>
2020-08-11 23:17:30 +0100

    fixed footer styling and content

--- src/Components/Footer/FooterAbout.js
+++ src/Components/Footer/FooterAbout.js
@@ -3,10 +3,18 @@ import { Col } from "react-bootstrap";
 
 export default function FooterAbout() {
   return (
-    <Col xs={12} sm={12} md={4} lg={4}>
+    <Col xs={12} sm={12} md={5} lg={5}>
         <h4 className="footer-headings">About</h4>
       <p>
-        Market Place is an online market for the Enugu community. Here, we are connecting people to the best stuck in the Enugu town.
+      Recruiters always complain that they don’t see portfolio-worthy-projects in most junior portfolio and so can't employ them.
+      </p>
+
+      <p>
+      Beginners/juniors are always confused as to what projects they should do as beginners that can attract recruiters.
+      </p>
+
+      <p>
+      Ideas Hub solves this problem as Recruiters and other advanced developers/designers can upload the kind of project that can make junior developers/designers employable.
       </p>
     </Col>
   );
--- src/Components/Footer/FooterContact.js
+++ src/Components/Footer/FooterContact.js
@@ -1,15 +1,26 @@
 import React from "react";
-import { Col, ListGroup } from "react-bootstrap";
+import { Col, Form, Button } from "react-bootstrap";
 
 export default function FooterContact() {
   return (
     <Col xs={12} sm={12} md={4} lg={4}>
-        <h4 className="footer-headings">Contact</h4>
-      <ListGroup variant="flush">
-        <ListGroup.Item>info@market.com</ListGroup.Item>
-        <ListGroup.Item>08012345678</ListGroup.Item>
-        <ListGroup.Item>Enugu, Enugu State</ListGroup.Item>
-      </ListGroup>
+      <h4 className="footer-headings">Contact</h4>
+
+      <Form>
+        {/* email */}
+        <Form.Group controlId="exampleForm.ControlInput1">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+
+        {/* message */}
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Message</Form.Label>
+          <Form.Control as="textarea" rows="5" />
+        </Form.Group>
+
+        <Button>Submit</Button>
+      </Form>
     </Col>
   );
 }
