814db2a9f1934eedb54ddb93768a9e956043fdf6
EBEREGIT <samsoneberenjoku@gmail.com>
2020-08-12 12:56:17 +0100

    added icon to the tooltip files

--- src/Components/Helpers/InstructionURLToolTip.js
+++ src/Components/Helpers/InstructionURLToolTip.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from "react";
 import { Overlay, Tooltip } from "react-bootstrap";
+import { IoIosHelpCircle } from "react-icons/io";
 
 export default function InstructionURLToolTip() {
   const [show, setShow] = useState(false);
@@ -9,12 +10,13 @@ export default function InstructionURLToolTip() {
     <>
       {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
       <a ref={target} onClick={() => setShow(!show)}>
-        ?
+        <IoIosHelpCircle />
       </a>
       <Overlay target={target.current} show={show} placement="right">
         {(props) => (
           <Tooltip id="overlay-example" {...props}>
-            It can be a link to a PDF document in a google drive. If you don't have this, please enter any other relevant link
+            It can be a link to a PDF document in a google drive. If you don't
+            have this, please enter any other relevant link
           </Tooltip>
         )}
       </Overlay>
--- src/Components/Helpers/SampleURLTooltip.js
+++ src/Components/Helpers/SampleURLTooltip.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from "react";
 import { Overlay, Tooltip } from "react-bootstrap";
+import { IoIosHelpCircle } from "react-icons/io";
 
 export default function SampleURLToolTip() {
   const [show, setShow] = useState(false);
@@ -9,12 +10,13 @@ export default function SampleURLToolTip() {
     <>
       {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
       <a ref={target} onClick={() => setShow(!show)}>
-        ?
+        <IoIosHelpCircle />
       </a>
       <Overlay target={target.current} show={show} placement="right">
         {(props) => (
           <Tooltip id="overlay-example" {...props}>
-            This is a URL of an example of the project you want the beginner to build. This will guide the beginner better
+            This is a URL of an example of the project you want the beginner to
+            build. This will guide the beginner better
           </Tooltip>
         )}
       </Overlay>
