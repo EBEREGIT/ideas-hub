b122fc2d12bcc373af128ee880b322dc79811a52
EBEREGIT <samsoneberenjoku@gmail.com>
2020-08-19 23:10:48 +0100

    Delete project now working

--- src/Components/Dashboard/Projects/EachProject.js
+++ src/Components/Dashboard/Projects/EachProject.js
@@ -4,8 +4,33 @@ import { RiDeleteBin5Line } from "react-icons/ri";
 import { IconContext } from "react-icons";
 import EditProject from "./EditProject";
 import ProjectDetails from "../../../ProjectDetails";
+import axios from "axios";
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
 
 export default function EachProject(props) {
+  // delete project here
+  const deleteProject = (id) => {
+    // get cookie from browser if logged in
+    const token = cookies.get("ONYE-NA-ENYO-ISI-YA-ANA-APUTA"),
+      method = "delete",
+      url = `https://ideas-app-api.herokuapp.com/projects/delete/${id}`,
+      headers = {
+        Authorization: `Bearer ${token}`,
+      };
+
+    // delete 
+    axios({ url, method, headers })
+      .then((result) => {
+        console.log(result);
+        // redirect user to the dashboard page
+        window.location.href = "/dashboard";
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
   return (
     <Col xs={12} sm={6} md={4} lg={4}>
       <Card style={{ width: "100%" }}>
@@ -48,7 +73,7 @@ export default function EachProject(props) {
           <IconContext.Provider
             value={{ style: { color: "red", fontSize: "1.5em" } }}
           >
-            <Button>
+            <Button onClick={() => deleteProject(props.id)}>
               <RiDeleteBin5Line />
             </Button>
           </IconContext.Provider>
